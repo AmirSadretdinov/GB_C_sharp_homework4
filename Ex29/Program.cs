aabd08831c35da69dc56e466fc711944fbc6f130
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-09-12 12:04:25 +0300

    add Ex29

---
+++ Ex29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void FillArray(int[] array)
+{   
+    for (int i = 0; i < array.Length; i++)
+    {   
+        Console.WriteLine("Введите элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+Console.WriteLine("Введите размерность массива");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] mas = new int[x];
+FillArray(mas);
+PrintArray(mas);
+
