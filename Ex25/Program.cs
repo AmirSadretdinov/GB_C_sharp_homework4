37492aa4476f07be9154ca35bbe7b895e00a5cc8
AmirSadretdinov <sadretdinov-amir@mail.ru>
2022-09-12 12:03:50 +0300

    add Ex25

---
+++ Ex25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Stepen(int A, int B)
+{   int result = 1;
+    if (B>0) return result = A * Stepen(A, B-1);
+    return 1;
+}
+System.Console.WriteLine("Введите А");
+int inputA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите В");
+int inputB = Convert.ToInt32(Console.ReadLine());
+double result = Stepen(inputA,inputB);
+System.Console.WriteLine($"{inputA}^{inputB} = {result}");
